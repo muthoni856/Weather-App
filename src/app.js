5a377e7c29f4cdc640e7b365b240e27c364bf47e
muthoni856 <wachira.joy@strathmore.edu>
2024-06-05 21:43:50 +0300

    API integration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
-function manageSearchInput(event){
-    event.preventDefault();
-    let searchInput=document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#search-city");
-    cityElement.innerHTML=searchInput.value;
+function updateWeather(response) {
+    console.log("API response received:", response); // Debugging line
+
+    let temperatureElement = document.querySelector("#weather-value");
+    let temperature = response.data.daily[0].temperature.day; 
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
 }
 
-let searchFormElement=document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",manageSearchInput);
+function searchCity(city) {
+    let apiKey = "6b025c7d6331b719f34f6a74oab04ft9";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+    
+
+    axios.get(apiUrl).then(updateWeather);
+        
+}
+
+function manageSearchInput(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", manageSearchInput);
